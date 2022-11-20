ab06bf97ea07fa13291a6452e10569236009c82e
sheldon <willian12345@126.com>
2022-11-20 22:22:30 +0800

    feat: Segment 类dom

--- src/js/Segment.ts
+++ src/js/Segment.ts
@@ -5,14 +5,40 @@
  *  <div class="segment-handle segment-handle-right"></div>
  * </div>
  */
-
+let segmentIdIndex = 0
 
 export class Segment {
   framestart = 0
   frameend = 0
   frames = 0
+  width = '80px'
+  height = '24px'
+  left = '0'
+  segmentId = ''
+  segmentClass = 'segment segment-action'
   dom = {} as HTMLElement
-  constructor(dom: HTMLElement){
-    this.dom = dom;
+  trackId = ''
+  constructor({trackId = ''}){
+    this.trackId = trackId
+    this.dom = this.createDom();
+  }
+  private createSegmentId() {
+    return segmentIdIndex++;
+  }
+  private createDom () {
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <div 
+          class="${this.segmentClass}" 
+          data-segment-id="${this.createSegmentId()}" 
+          data-track-id="${this.trackId}" 
+          data-framestart="${this.framestart}" 
+          data-frameend="${this.frameend}"
+          style="width: ${this.width}; height: ${this.height}; left: ${this.left};">
+          <div class="segment-handle segment-handle-left"></div>
+          <div class="segment-handle segment-handle-right"></div>
+        </div>
+      `;
+    return div.firstElementChild as HTMLElement;
   }
 }
