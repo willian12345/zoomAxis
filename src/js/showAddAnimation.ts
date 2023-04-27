f4dd4e9edd5d7814531024e82e61ed808342b77d
willian12345 <willian12345@126.com>
2023-04-27 19:13:16 +0800

    fix: change segment add animation ratios

--- src/js/showAddAnimation.ts
+++ src/js/showAddAnimation.ts
@@ -4,31 +4,61 @@ const createMoveDiv = (rect: DOMRect) => {
   return div;
 };
 const getLastSegment = (track: HTMLElement) => {
-  if(!track){
+  if (!track) {
     return null;
   }
-  const segmentList = Array.from(track.querySelectorAll('.segment')) as HTMLElement[];
-  if(!segmentList.length){
+  const segmentList = Array.from(
+    track.querySelectorAll(".segment")
+  ) as HTMLElement[];
+  if (!segmentList.length) {
     return null;
   }
   let last = segmentList[0];
   let frameend = 0;
   segmentList.forEach((s) => {
-    const currentFrameend = parseInt(s.dataset.frameend ?? '0');
-    if(currentFrameend > frameend){
+    const currentFrameend = parseInt(s.dataset.frameend ?? "0");
+    if (currentFrameend > frameend) {
       frameend = currentFrameend;
-      last = s
+      last = s;
     }
   });
   return last;
-}
-export const showAnimation = async (delegatedDom: HTMLElement, targetId: string) => {
+};
+// 获取系数 a
+const getAratioByDistanceX = (distanceX: number) => {
+  distanceX = Math.abs(distanceX);
+  if (distanceX < 400) {
+    return 0.4;
+  }
+  // 距离太远高度太高会没有意义，直接返回 0
+  if (distanceX > 4000) {
+    return 0;
+  }
+  // 目标距离越远，a 系数越小
+  return 1 / distanceX;
+};
+// 获取动画时长 ms 单位
+const getDurationByDistanceX = (distanceX: number) => {
+  distanceX = Math.abs(distanceX);
+  // 距离小或太大
+  if (distanceX < 400 || distanceX > 4000) {
+    return 400;
+  }
+  // 根据距离调整时长
+  return distanceX * 0.4;
+};
+export const showAnimation = async (
+  delegatedDom: HTMLElement,
+  targetId: string
+) => {
   return new Promise((resolve) => {
     const rect = delegatedDom.getBoundingClientRect();
     let elX = rect.left;
     let elY = rect.top;
-    const track = document.getElementById('videoEditorTimelineContainer')?.querySelector(`div[data-track-id='${targetId}']`) as HTMLElement;
-    if(!track || track.style.display === 'none'){
+    const track = document
+      .getElementById("videoEditorTimelineContainer")
+      ?.querySelector(`div[data-track-id='${targetId}']`) as HTMLElement;
+    if (!track || track.style.display === "none") {
       resolve(true);
       return;
     }
@@ -43,11 +73,11 @@ export const showAnimation = async (delegatedDom: HTMLElement, targetId: string)
     let distanceX = targetRect.left - elX;
     let distanceY = targetRect.top - elY;
     // 如果轨道内有添加过，则目标为最后一个 segment 的结束帧位置
-    if(segmentDom){
+    if (segmentDom) {
       distanceX += targetRect.width;
     }
-    const a = 0.001; // todo: 通过屏宽与 distanceX 动态更新 a 系数
-    const duration = 400; // todo: 通过屏宽与 distanceX 动态更新 duration
+    const a = getAratioByDistanceX(distanceX);
+    const duration = getDurationByDistanceX(distanceX);
     const b = (distanceY - a * distanceX * distanceX) / distanceX;
     let timer: number;
     let endTime = 0;
@@ -60,11 +90,11 @@ export const showAnimation = async (delegatedDom: HTMLElement, targetId: string)
     function step() {
       let x = 0;
       let y = 0;
-      const now: number = (+new Date())
+      const now: number = +new Date();
       if (now > endTime) {
         x = distanceX;
         y = distanceY;
-        window.cancelAnimationFrame(timer)
+        window.cancelAnimationFrame(timer);
         current.parentElement?.removeChild(current);
         resolve(true);
       } else {
@@ -73,7 +103,7 @@ export const showAnimation = async (delegatedDom: HTMLElement, targetId: string)
       }
       current.style.left = `${elX + x}px`;
       current.style.top = `${elY + y}px`;
-      requestAnimationFrame(step)
+      requestAnimationFrame(step);
     }
     start();
   });
